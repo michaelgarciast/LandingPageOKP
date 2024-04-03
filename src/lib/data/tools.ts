15ca8c12580283aa5281048cb98188c6521ac407
Michael Steven García santa <michael.garcia@linktic.com>
2024-04-03 16:17:31 -0500

    commit update tools

---
+++ src/lib/data/tools.ts
@@ -0,0 +1,13 @@
+export const dataTools = [
+  { name: 'AWS', image_link: '/images/tools/aws.png' },
+  { name: 'Docker', image_link: '/images/tools/docker.png' },
+  { name: 'Firebase', image_link: '/images/tools/firebase.png' },
+  { name: 'Flutter', image_link: '/images/tools/flutter.png' },
+  { name: 'Golang', image_link: '/images/tools/go.png' },
+  { name: 'Laravel', image_link: '/images/tools/laravel.png' },
+  { name: 'MongoDB', image_link: '/images/tools/mongodb.png' },
+  { name: 'NextJS', image_link: '/images/tools/nextjs.png' },
+  { name: 'NodeJS', image_link: '/images/tools/node.png' },
+  { name: 'React', image_link: '/images/tools/react.png' },
+  { name: 'Svelte', image_link: '/images/tools/svelte.png' },
+];
